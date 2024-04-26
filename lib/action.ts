71a12325201d1abbed171db7598c7770c2c59a13
CojaDev <amarcetic04@gmail.com>
2024-04-26 05:04:01 +0200

    added featured product

--- lib/action.ts
+++ lib/action.ts
@@ -1,20 +1,16 @@
-import { mongooseConnect } from './mongoose';
 import axios from 'axios';
 
 export const getProducts = async () => {
-  await mongooseConnect();
   const products = await axios.get('/api/products');
   return products.data;
 };
 
 export const getCategories = async () => {
-  await mongooseConnect();
   const categories = await axios.get('/api/categories');
   return categories.data;
 };
 
 export const getStore = async () => {
-  await mongooseConnect();
   const store = await axios.get('/api/store');
   return store.data;
 };
