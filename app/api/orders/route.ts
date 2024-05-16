344f7a3c75621a0594ff6526c4ad5e7ab259eea3
CojaDev <amarcetic04@gmail.com>
2024-05-16 16:15:56 +0200

    added checkout and payment(stripe)2

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -1,10 +1,6 @@
 import Orders from '../../models/Orders';
 import { mongooseConnect } from '../../../lib/mongoose';
 import { NextRequest, NextResponse } from 'next/server';
-import Stripe from 'stripe';
-export const stripe = new Stripe(process.env.NEXT_PUBLIC_SECRET_STRIPE_KEY!, {
-  typescript: true,
-});
 
 const updateIndexes = async () => {
   try {
@@ -18,9 +14,9 @@ const updateIndexes = async () => {
   }
 };
 
-//updateIndexes();
+updateIndexes();
 
-export async function PUT(request: any) {
+export async function POST(request: any) {
   try {
     await mongooseConnect();
     const {
@@ -54,60 +50,5 @@ export async function PUT(request: any) {
       { message: 'Failed to create Order' },
       { status: 500 }
     );
-  }
-}
-
-const corsHeaders = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-};
-
-export async function OPTIONS() {
-  return NextResponse.json({}, { headers: corsHeaders });
-}
-
-export async function POST(req: NextRequest) {
-  try {
-    const { cartItems, customer } = await req.json();
-
-    if (!cartItems || !customer) {
-      return new NextResponse('Not enough data to checkout', { status: 400 });
-    }
-
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      mode: 'payment',
-      shipping_address_collection: {
-        allowed_countries: ['US', 'CA'],
-      },
-      shipping_options: [
-        { shipping_rate: 'shr_1MfufhDgraNiyvtnDGef2uwK' },
-        { shipping_rate: 'shr_1OpHFHDgraNiyvtnOY4vDjuY' },
-      ],
-      line_items: cartItems.map((cartItem: any) => ({
-        price_data: {
-          currency: 'cad',
-          product_data: {
-            name: cartItem.item.title,
-            metadata: {
-              productId: cartItem.item._id,
-              ...(cartItem.size && { size: cartItem.size }),
-              ...(cartItem.color && { color: cartItem.color }),
-            },
-          },
-          unit_amount: cartItem.item.price * 100,
-        },
-        quantity: cartItem.quantity,
-      })),
-      client_reference_id: customer.clerkId,
-      success_url: `${process.env.STORE_URL}/payment_success`,
-      cancel_url: `${process.env.STORE_URL}/cart`,
-    });
-
-    return NextResponse.json(session, { headers: corsHeaders });
-  } catch (err) {
-    console.log('[checkout_POST]', err);
-    return new NextResponse('Internal Server Error', { status: 500 });
   }
 }
