076461e3695b7ba80ba2015b84b6aea991f88ac3
CojaDev <amarcetic04@gmail.com>
2024-05-31 13:52:40 +0200

    orders update

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -3,32 +3,15 @@ import Stripe from 'stripe';
 import { mongooseConnect } from '../../../lib/mongoose';
 import Orders from '../../models/Orders';
 
-const stripe = new Stripe(process.env.NEXT_PUBLIC_SECRET_STRIPE_KEY!, {
-  typescript: true,
-});
-
-export async function POST(req: NextRequest) {
-  const { session_id } = await req.json();
-
+export async function GET(request: NextRequest) {
   try {
-    const session = await stripe.checkout.sessions.retrieve(session_id);
-
-    // Ensure the order has been saved by the webhook
     await mongooseConnect();
-    const order = await Orders.findOne({
-      'recipient.email': session.customer_details!.email,
-      status: 'Paid',
-    });
-
-    if (!order) {
-      return NextResponse.json({ error: 'Order not found' }, { status: 404 });
-    }
-
-    return NextResponse.json({ order }, { status: 200 });
+    const order = await Orders.find();
+    return NextResponse.json(order);
   } catch (error) {
-    console.error('Error completing order:', error);
+    console.error('Error fetching order:', error);
     return NextResponse.json(
-      { error: 'Internal Server Error' },
+      { message: 'Failed to fetch order' },
       { status: 500 }
     );
   }
